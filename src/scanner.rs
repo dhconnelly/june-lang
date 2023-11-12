42747eb76680825fe5619603e9d355dbbf240889
Daniel Connelly <dhconnelly@gmail.com>
2023-11-12 18:24:42 +0100

    cleaned up scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -61,21 +61,21 @@ fn ident_type(s: &str) -> TokenType {
 
 impl<R: io::BufRead> Scanner<R> {
     fn peek(&mut self) -> Option<Result<u8>> {
-        // if peek has an error, go ahead and get the owned result via advance
+        // not all errors can be copied (e.g. io::Error), so if peek has
+        // an error, we go ahead and get the owned error via advance().
         match self.bytes.peek() {
-            None => None,
-            Some(Err(_)) => Some(self.bytes.next()?.map_err(|e| e.into())),
+            Some(Err(_)) => Some(Err(self.advance().unwrap_err())),
             Some(Ok(b)) => Some(Ok(*b)),
+            None => None,
         }
     }
 
     fn advance(&mut self) -> Result<u8> {
         let b = self.bytes.next().ok_or(Error::UnexpectedEOF)??;
+        self.col += 1;
         if b == b'\n' {
             self.line += 1;
             self.col = 1;
-        } else {
-            self.col += 1;
         }
         Ok(b)
     }
@@ -90,8 +90,7 @@ impl<R: io::BufRead> Scanner<R> {
     fn eat_while(&mut self, f: impl Fn(u8) -> bool) -> Result<String> {
         let mut buf = Vec::new();
         while f(self.peek().ok_or(Error::UnexpectedEOF)??) {
-            let b = self.advance().unwrap();
-            buf.push(b);
+            buf.push(self.advance().unwrap());
         }
         Ok(String::from_utf8(buf)?)
     }
@@ -115,6 +114,12 @@ impl<R: io::BufRead> Scanner<R> {
         let text = self.eat_while(|b| !is_delim(b))?;
         Ok(Token { typ: ident_type(&text), text, line, col })
     }
+
+    fn skip_whitespace(&mut self) {
+        while matches!(self.peek(), Some(Ok(b' ' | b'\n'))) {
+            self.advance().unwrap();
+        }
+    }
 }
 
 #[derive(Debug, Error)]
@@ -129,17 +134,11 @@ impl<R: io::BufRead> iter::Iterator for Scanner<R> {
     type Item = result::Result<Token, ScannerError>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let result = loop {
-            let (line, col) = (self.line, self.col);
-            let b = match self.peek()? {
-                Ok(b) => b,
-                Err(err) => break Err(ScannerError { line, col, err }),
-            };
-            let result = match b {
-                b if b.is_ascii_whitespace() => {
-                    self.advance().unwrap();
-                    continue;
-                }
+        self.skip_whitespace();
+        let (line, col) = (self.line, self.col);
+        let result = self
+            .peek()?
+            .and_then(|b| match b {
                 b'(' => self.symbol(TokenType::Lparen),
                 b')' => self.symbol(TokenType::Rparen),
                 b'{' => self.symbol(TokenType::Lbrace),
@@ -149,12 +148,8 @@ impl<R: io::BufRead> iter::Iterator for Scanner<R> {
                 b'"' => self.str(),
                 b if b.is_ascii_alphabetic() => self.keyword_or_ident(),
                 b => Err(Error::UnknownToken(b)),
-            };
-            break match result {
-                Err(err) => Err(ScannerError { line, col, err }),
-                Ok(tok) => Ok(tok),
-            };
-        };
+            })
+            .map_err(|err| ScannerError { line, col, err });
         Some(result)
     }
 }
@@ -168,8 +163,7 @@ mod tests {
     use super::*;
 
     fn scan_all(b: &[u8]) -> result::Result<Vec<Token>, ScannerError> {
-        let scanner = scan(b);
-        scanner.collect()
+        scan(b).collect()
     }
 
     fn tok(typ: TokenType, text: &str, line: usize, col: usize) -> Token {
