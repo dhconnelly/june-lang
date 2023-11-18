157f4420a56800ce8bbb06330cbd57a80c270fca
Daniel Connelly <dhconnelly@gmail.com>
2023-11-18 11:41:32 +0100

    emit an empty module

--- src/emitter.rs
+++ src/emitter.rs
@@ -1,13 +1,23 @@
 use crate::wasm::*;
+use std::io;
 use std::io::Write;
 use std::result;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum Error {
+    #[error("io: {0}")]
+    IOError(#[from] io::Error),
+}
 
 pub type Result<T> = result::Result<T, Error>;
 
-pub fn emit<W: Write>(_w: W, _wasm: &[Wasm]) -> Result<()> {
+// https://webassembly.github.io/spec/core/binary/modules.html#binary-module
+const HEADER: &'static [u8] = &[0x00, 0x61, 0x73, 0x6d];
+const VERSION: &'static [u8] = &[0x01, 0x00, 0x00, 0x00];
+
+pub fn emit<W: Write>(mut w: W, _wasm: &[Wasm]) -> Result<()> {
+    w.write_all(HEADER)?;
+    w.write_all(VERSION)?;
     Ok(())
 }
