221c6430a52a1de34f2fd7dee2c01e5545cd33a4
Daniel Connelly <dhconnelly@gmail.com>
2024-01-22 21:42:06 +0100

    fix execution

--- src/bin/junevm.rs
+++ src/bin/junevm.rs
@@ -1,6 +1,6 @@
 fn main() {
     for arg in std::env::args().skip(1) {
-        if let Err(err) = june_lang::driver::execute(&arg) {
+        if let Err(err) = june_lang::driver::execute_file(&arg) {
             eprintln!("junec: {}", err);
             std::process::exit(1);
         }
